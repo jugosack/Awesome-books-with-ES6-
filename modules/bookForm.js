ebd0d067396b341e74d328a037cd794376c95a5b
jugosack <jugoslav_ackoski@yahoo.com>
2023-01-17 12:21:31 +0100

    Creating js files

---
+++ modules/bookForm.js
@@ -0,0 +1,19 @@
+const bookForm = document.getElementById('books');
+  bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    if (!title || !author) {
+      alert('Please enter a Title and Author first');
+      return;
+    }
+    const book = new ShowBooks();
+    book.displayBook({ title, author });
+    book.addBook({ title, author });
+  
+    title.value = '';
+    author.value = '';
+  });
+
+  const toDisplay = new ShowBooks();
+  toDisplay.iterateBooks();
